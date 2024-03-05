803eb309a8a86ee756c38a477970e2e038f4a84a
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-05 19:04:23 +0700

    menambahkan nilai tugas

---
+++ TugasNilai_203.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float matematika, fisika, rerata;
+    string status;
+    cout <<"nilai matematika : " << endl;
+    cin >> matematika;
+    cout << "nilai fisika : " << endl;
+    cin >> fisika;
+    rerata = (matematika + fisika) /2;
+    if (matematika >= 70 )
+    {
+        status = "lulus";
+    }
+    else if (rerata > 60)
+    {
+        status = "lulus";
+    }
+    else 
+    {
+        status = "gagal";
+    }
+    
+    cout << "rerata = " << rerata << endl;
+    cout <<"status kelullusan = " << status << endl;
+
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float matematika, fisika, rerata;
+    string status;
+    cout <<"nilai matematika : " << endl;
+    cin >> matematika;
+    cout << "nilai fisika : " << endl;
+    cin >> fisika;
+    rerata = (matematika + fisika) /2;
